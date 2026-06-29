97d2758f49c9f40beda38a52945d295e8f5d5c22
Sourabh Kumar <sourabhsingh0609@gmail.com>
2026-06-30 00:07:23 +0530

    Add main entry point, config flags, and TCP server skeleton

---
+++ config/config.go
@@ -0,0 +1,7 @@
+package config
+
+const DEFAULT_PORT = 7379
+const DEFAULT_HOST = "0.0.0.0"
+
+var Host string
+var Port int
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/sourabh-kumar2/go-redis/config"
+)
+
+func setupFlags() {
+	flag.StringVar(&config.Host, "host", config.DEFAULT_HOST, "host for the server")
+	flag.IntVar(&config.Port, "port", config.DEFAULT_PORT, "port for the server")
+	flag.Parse()
+}
+
+func main() {
+	setupFlags()
+
+	log.Println("rolling the server")
+}
